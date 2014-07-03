3200fd3d3b8d0a921d9956d8c6dd425bb338627b
mattiag94 <matti.graziani94@gmail.com>
2014-07-03 20:21:54 +0200

    [MEDIA] Removed overwrite for font configuration

--- DonUI/media/medias.lua
+++ DonUI/media/medias.lua
@@ -1,5 +1,3 @@
 local T, C, L, G = unpack(Tukui)
 
-C.media.uffont = [[Interface\AddOns\DonUI\media\fonts\pixel.ttf]]
-C.media.dmgfont = [[Interface\AddOns\DonUI\media\fonts\pixel.ttf]]
-C.media.uffont = [[Interface\AddOns\DonUI\media\fonts\pixel.ttf]]
+-- everything in here will override default configs. Use this file or custom media loading
